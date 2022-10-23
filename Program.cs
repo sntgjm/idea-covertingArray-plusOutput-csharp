dc3eba831fc835a6be1d6d5eaab4154c76166eaa
Jm Santiago <116279303+sntgjm@users.noreply.github.com>
2022-10-23 14:19:02 +0800

    Activity 2

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new int[1];
+            number[0] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The number is " + number[0]);
+        }
+    }
+}
